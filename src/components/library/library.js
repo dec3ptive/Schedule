7d489c375777704485d9bea5a6eed1b6c9aaa34c
Quinn Ormond <quinnormond2000@gmail.com>
2018-07-26 10:46:21 -0600

    built gradient component

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -3,7 +3,9 @@ import React, { component} from 'react';
 class Library extends Component {
     render() {
         <div className="library">
-        
+            <h1 className="library__title">
+                Course Library
+            </h1>
         </div>
     }
 }
---
+++ src/components/schedule/schedule.js
@@ -0,0 +1,13 @@
+import React, { component} from 'react';
+import Gradient from '../gradient';
+
+class Schedule extends Component {
+    render() {
+        <div className="library">
+            <h1 className="schedule__title">My schedule</h1>
+            <Gradient/>
+        </div>
+    }
+}
+
+export default Schedule;
