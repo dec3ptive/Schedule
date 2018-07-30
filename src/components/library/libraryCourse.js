313448246b8cd25064c7b64667776de4c73b5817
Quinn Ormond <quinnormond2000@gmail.com>
2018-07-30 09:50:42 -0600

    library course component grid finished

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Icon from '../icon';
+import Arrow from '../arrow';
+import Action from '../action';
 
 class LibraryCourse extends Component {
     render () {
         return (
             <div className="library-course">
-                <label className="library-course__title">Problem Solving</label>
-                { Icon('fas fa-check', "library-course__icon") }
-                {/* arrow component */}
-                {/* action button component */}
+                <div className="library-course__title-check">
+                    <label className="library-course__title">Problem Solving</label>
+                    { Icon('fas fa-check', "library-course__icon") }
+                </div>
+                <Arrow className= "library-cour__arrow"/>
+                <Action className="library-course__action"/>
                 <div className="library-course__description">
                     <label> course__description</label>
                     <p>Sometimes chicken sounds like it could just not when you start typing random threads into the word box to the end of this filler line</p>
